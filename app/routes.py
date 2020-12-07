e7a91d36eee1bf29237f18731b8dc180d944ea0a
Arthur Shchipletcov <geexmmo@gmail.com>
2020-12-08 01:43:41 +0600

    test jinja2 template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
+def index0():
+    return render_template('index.html', title='/')
+
 @app.route('/index')
-def index ():
-    return 'Hi there'
+def index():
+    user = {'username':'geexmmo'}
+    devices = [
+    {'number':'1001','address':{'mac':'DE:AD:BE:EF:FF'}},
+    {'number':'1002','address':{'mac':'FF:FE:EB:DA:ED'}},
+    {'number':'1003','address':{'mac':'FF:FF:EE:FF:FF'}}]
+    return render_template('index.html', title='Home', user=user, devices=devices)
