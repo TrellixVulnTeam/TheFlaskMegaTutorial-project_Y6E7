e819ccf41db314bd52a70adaf493c0fc513e352d
Arthur Shchipletcov <geexmmo@gmail.com>
2020-12-08 02:24:47 +0600

    new top bar and template redesign

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,15 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-def index0():
-    return render_template('index.html', title='/')
-
 @app.route('/index')
 def index():
+    links = [
+        {'name':'Add','href':'/newdevice'},
+        {'name':'List','href':'/index'}
+    ]
     user = {'username':'geexmmo'}
     devices = [
-    {'number':'1001','address':{'mac':'DE:AD:BE:EF:FF'}},
-    {'number':'1002','address':{'mac':'FF:FE:EB:DA:ED'}},
-    {'number':'1003','address':{'mac':'FF:FF:EE:FF:FF'}}]
-    return render_template('index.html', title='Home', user=user, devices=devices)
+        {'number':'1001','address':{'mac':'DE:AD:BE:EF:FF'}},
+        {'number':'1002','address':{'mac':'FF:FE:EB:DA:ED'}},
+        {'number':'1003','address':{'mac':'FF:FF:EE:FF:FF'}}]
+    return render_template('index.html', title='Home', user=user, links=links, devices=devices)
