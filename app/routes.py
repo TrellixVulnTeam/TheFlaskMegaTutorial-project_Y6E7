f20c9cd49455edd55e04bde07e6b8dd65ebde828
Arthur Shchipletcov <geexmmo@gmail.com>
2020-12-09 01:49:42 +0600

    basic forms implementation

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me {}'.format(
+            form.username.data, form.remember_me.data))
+        flash('Login dummy')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/')
 @app.route('/index')
 def index():
     links = [
-        {'name':'Add','href':'/newdevice'},
-        {'name':'List','href':'/index'}
+        {'name':'Login','class':'login'},
+        {'name':'Add','class':'inprogress'},
+        {'name':'Poop','class':'inprogress'},
+        {'name':'Le booze','class':'inprogress'},
+        {'name':'List','class':'index'}
     ]
     user = {'username':'geexmmo'}
     devices = [
@@ -14,3 +28,7 @@ def index():
         {'number':'1002','address':{'mac':'FF:FE:EB:DA:ED'}},
         {'number':'1003','address':{'mac':'FF:FF:EE:FF:FF'}}]
     return render_template('index.html', title='Home', user=user, links=links, devices=devices)
+
+@app.route('/inprogress')
+def inprogress():
+    return render_template('inprogress.html')
