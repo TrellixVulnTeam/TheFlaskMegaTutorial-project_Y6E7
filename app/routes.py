14305a0d7b20f4eb4e9b67b0d7439c67cc841d5d
geexmmo <geexmmo@gmail.com>
2020-12-17 18:17:00 +0600

    user panel and registration

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
+from app import app, db
 from app.models import User, Post
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 
 @app.route('/login', methods=['GET','POST'])
@@ -22,10 +22,34 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congradulations, you are now a registred user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post 1'},
+        {'author': user, 'body': 'Test post 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/')
 @app.route('/index')
