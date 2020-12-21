f48c72451d1e459aadb76749ebc28920011dc171
geexmmo <geexmmo@gmail.com>
2020-12-21 18:26:56 +0600

    user edit impovements, started to implement "following" funcion

--- config.py
+++ config.py
@@ -3,5 +3,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 's3cur1t3y'
+    # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # ...
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "localhost"
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ... mail_to
+    MAIL_ADMIN = os.environ.get('MAIL_ADMIN') or ['admin@example.com']
