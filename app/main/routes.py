a4e6c179218fb1c39a8be4ca3857ac74da800011
geexmmo <geexmmo@gmail.com>
2021-03-29 17:58:35 +0600

    export functionality completed, user presentation in form of button is undone

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from os import link
 from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
 from flask_login import current_user, login_required
+from flask_migrate import current
 from app import db
 from app.main import bp
 from app.models import User, Post, Message, Notification
@@ -208,6 +209,16 @@ def notifications():
         'data': n.get_data(),
         'timestamp': n.timestamp
     } for n in notifications])
+
+@bp.route('/export_posts')
+@login_required
+def export_posts():
+    if current_user.get_task_in_progress('export_posts'):
+        flash(_('An export task is currently in progress'))
+    else:
+        current_user.launch_task('export_posts', _('Exporting posts...'))
+        db.session.commit()
+    return redirect(url_for('main.user', username=current_user.username))
 
 @bp.route('/inprogress')
 def inprogress():
